6fb60595b1b86ebf4a06c9c822b87aa1b2e5194d
Zenerboson <zenerboson@gmail.com>
2015-05-24 16:52:25 -0700

    MUD
    Fully working, now time to expand!

--- src/libadventure.py
+++ src/libadventure.py
@@ -1,5 +1,4 @@
 #!C:\Python27
-
 class World:
 	def __init__(self,initialroom):
 		self.rooms={}
@@ -7,7 +6,7 @@ class World:
 		self.rooms[initialroom.name]=initialroom
 		self.spawn=self.rooms[initialroom.name]
 	def add_room(self,room):
-		self.rooms.append(room)
+		self.rooms[room.name]=room
 	def add_player(self,player):
 		self.players[player.name]=player
 		self.spawn.players[player.name]=player
@@ -15,17 +14,68 @@ class World:
 	def move_player(self,room1,room2,playername):
 		room2.players[playername]=room1.players[playername]
 		room2.players[playername].room=room2
-		del room1.players[]
+		del room1.players[playername]
+	def process_command(self,command,playername):
+		player=self.players[playername]
+		extra=""
+		if command=="look" or command=="l":
+			for key,value in player.room.contents.iteritems():
+				extra+="\nYou can also see a "+key+" here"
+			for key,value in player.room.players.iteritems():
+				if key!=player.name:
+					extra+="\n%s is here too!" % key
+			return player.room.name+" : "+player.room.appearance+extra
+		elif command=="west" or command=="w" or command=="go w" or command=="go west":
+			if player.room.west!=None:
+				self.move_player(player.room,self.rooms[player.room.west],player.name)
+				return player.room.name+" : "+player.room.appearance
+			else:
+				return "You can't go that way!"
+		elif command=="east" or command=="e" or command=="go e" or command=="go east":
+			if player.room.east!=None:
+				self.move_player(player.room,self.rooms[player.room.east],player.name)
+				return player.room.name+" : "+player.room.appearance
+			else:
+				return "You can't go that way!"
+		elif command=="north" or command=="n" or command=="go n" or command=="go north":
+			if player.room.north!=None:
+				self.move_player(player.room,self.rooms[player.room.north],player.name)
+				return player.room.name+" : "+player.room.appearance
+			else:
+				return "You can't go that way!"
+		elif command=="south" or command=="s" or command=="go s" or command=="go south":
+			if player.room.south!=None:
+				self.move_player(player.room,self.rooms[player.room.south],player.name)
+				return player.room.name+" : "+player.room.appearance
+			else:
+				return "You can't go that way!"
+		elif command=="up" or command=="u" or command=="go u" or command=="go up":
+			if player.room.up!=None:
+				self.move_player(player.room,self.rooms[player.room.up],player.name)
+				return player.room.name+" : "+player.room.appearance
+			else:
+				return "You jump fruitlessly."
+		elif command=="down" or command=="d" or command=="go d" or command=="go down":
+			if player.room.down!=None:
+				self.move_player(player.room,self.rooms[player.room.down],player.name)
+				return player.room.name+" : "+player.room.appearance
+			else:
+				return "You can't go that way!"
+		else:
+			return "I'm not sure I understand you"
 class Room:
-	def __init__(self,name,appearance,contents={}):
+	def __init__(self,name,appearance,contents={},west=None,east=None,north=None,south=None,up=None,down=None):
 		self.appearance=appearance
 		self.name=name
 		self.contents=contents
 		self.players={}
+		self.west=west
+		self.east=east
+		self.north=north
+		self.south=south
+		self.up=up
+		self.down=down
 class Player:
 	def __init__(self,name):
 		self.inventory={}
-		
-
-
-playername
+		self.name=name
--- src/networking.py
+++ src/networking.py
@@ -2,21 +2,41 @@
 from twisted.internet import reactor, protocol, endpoints
 from twisted.protocols import basic
 import libadventure
-world=libadventure.World()
-
+room1=libadventure.Room("Spawn","You are in a plain nondescript room with a single bare lightbulb hanging from the ceiling. A dark and forbidding hallway leads west out of the room.",west="Dark Hallway")
+room2=libadventure.Room("Dark Hallway","You are in a dark hallway that leads east-to-west. It opens on a lit door to the east and continues into darkness on the west.",east="Spawn")
+world=libadventure.World(room1)
+world.add_room(room2)
 class GameProtocol(basic.LineReceiver):
     def __init__(self, factory):
         self.factory = factory
-
+        self.state=""
     def connectionMade(self):
         self.factory.clients.add(self)
-
+        self.state="USERNAME"
+        self.sendLine(b'Enter a username > ')
+        self.peer=self.transport.getPeer()
+        print("Connection recieved from %s" % str(self.transport.getPeer()))
+        print("Requesting username")
     def connectionLost(self, reason):
         self.factory.clients.remove(self)
+        print("Connection lost from %s" % str(self.peer))
 
     def lineReceived(self, line):
-        for c in self.factory.clients:
-            c.sendLine("<{}> {}".format(self.transport.getHost(), line))
+    	if self.state=="PLAYING":
+    		self.sendLine(world.process_command(line.decode('utf8'),self.username).encode('utf8'))
+    	if self.state=="USERNAME":
+    		self.username=line.decode('utf8')
+    		self.sendLine(b'Welcome, %s!' % self.username.encode('utf8'))
+    		self.sendLine(b'Spawn : You are in a plain nondescript room with a single bare lightbulb hanging from the ceiling. A dark and forbidding hallway leads west out of the room.')
+    		for c in self.factory.clients:
+    			if c!=self:
+    				c.sendLine(b'%s has joined the game!' % self.username.encode('utf8'))
+    		print("Client at %s gave username %s, logged in" % (self.peer,self.username))
+    		world.add_player(libadventure.Player(self.username))
+    		self.state="PLAYING"
+    	
+        #for c in self.factory.clients:
+          #  c.sendLine("<{}> {}".format(self.transport.getHost(), world.commandProcessor.process(line,self.transport.getHost())))
 
 class GameFactory(protocol.Factory):
     def __init__(self):
