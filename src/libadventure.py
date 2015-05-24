a33933ed3041f5faaafe3be58208a6d4290952da
Zenerboson <zenerboson@gmail.com>
2015-05-24 11:18:39 -0700

    Fixed some random crap
    sdsf

--- src/libadventure.py
+++ src/libadventure.py
@@ -1,4 +1,18 @@
 #!C:\Python27
 
 class World:
-	
+	def __init__(self,initialroom):
+		self.rooms={}
+		self.players={}
+		self.rooms[initialroom.name]=initialroom
+		self.spawn=self.rooms[initialroom.name]
+	def add_room(self,room):
+		self.rooms.append(room)
+	def add_player(self,player):
+		self.players[player.name]=player
+		self.spawn.players[player.name]=player
+	def move_player(self,room1,room2,playername):
+		room1.players[]
+		
+
+
