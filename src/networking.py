289224a17e590b4b2422b402988bf92567c45df0
Zenerboson <zenerboson@gmail.com>
2015-05-24 12:31:06 -0700

    Bleh
    sdsf

--- src/networking.py
+++ src/networking.py
@@ -1,1 +1,14 @@
 #!C:\Python27
+from twisted.internet import protocol, reactor, endpoints
+import libadventure
+
+class Echo(protocol.Protocol):
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+class EchoFactory(protocol.Factory):
+    def buildProtocol(self, addr):
+        return Echo()
+
+endpoints.serverFromString(reactor, "tcp:1234").listen(EchoFactory())
+reactor.run()
