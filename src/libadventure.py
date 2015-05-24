4356e52130e4fe35408258a03cfe181b64909b51
Zenerboson <zenerboson@gmail.com>
2015-05-24 12:05:46 -0700

    Finished base classes, starting on networking
    sdsf

--- src/libadventure.py
+++ src/libadventure.py
@@ -11,8 +11,20 @@ class World:
 	def add_player(self,player):
 		self.players[player.name]=player
 		self.spawn.players[player.name]=player
+		self.spawn.players[player.name].room=self.spawn
 	def move_player(self,room1,room2,playername):
-		room1.players[]
+		room2.players[playername]=room1.players[playername]
+		room2.players[playername].room=room2
+		del room1.players[playername]
+class Room:
+	def __init__(self,name,appearance,contents={}):
+		self.appearance=appearance
+		self.name=name
+		self.contents=contents
+		self.players={}
+class Player:
+	def __init__(self,name):
+		self.inventory={}
 		
 
 
