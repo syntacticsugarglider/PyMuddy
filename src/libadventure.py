7ee566ca795cc7e4d6d88bf0b33a7a7755d76fda
Zenerboson <zenerboson@gmail.com>
2015-11-19 15:41:30 -0800

    Moved extra functions to outside locals lib
    Exactly what I said

--- src/libadventure.py
+++ src/libadventure.py
@@ -4,26 +4,7 @@ import libinventory
 import libitems
 from datetime import datetime
 import sys
-def searchForItemInHashTable(itemName,dictionary):
-	names=itemName.split(' ')
-	if itemName=='' or itemName==' ':
-		return None
-	for name in names:
-		name=name.lower()
-		log('LOGGINGITEMS %s ALSO %s ' % (str([i.split(' ') for i in dictionary.keys()][0]),str(names)))
-		if name in [i.split(' ') for i in dictionary.keys()][0]:
-			items=[value for key, value in dictionary.items() if names[0] in key.lower()]
-			if len(items)==1:
-				item=items[0]
-				if len(item)==1:
-					final_item=item[0]
-					return ('single',final_item)
-				else:
-					return ('multi',item)
-			else:
-				concat_list=[item for sublist in items for item in sublist]
-				return('multi',concat_list)
-	return None
+from extensionlocals import *
 class CommandParser:
 	def __init__(self,world):
 		self.commands={}
